d52d1a6118d5f585bb90b0267e586b73b5a7fd1a
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2021-03-11 21:16:01 +0530

    Create 2variables.c

---
+++ 2variables.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 5;
+
+    // int b = 7.7; --> not recommended because 7.7 is not an integer
+
+    float b = 7.7;
+
+    char c = 'm';
+
+    int d = 50;
+    int e = 100 + 50;
+    // for int --> %d;
+    // for float --> %f;
+    // for char --> %c;
+
+    printf("The value of a is %d \n", a);
+    printf("The value of b is %f \n", b);
+    printf("The value of c is %c \n", c);
+    printf("Sum of a and d is %d \n", a + d);
+    printf("Sum operation performed at int e is %d \n", e);
+    
+    return 0;
+
+}
