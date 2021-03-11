6a0e6e01869be7f06c6d8a28e27147eb4c17bb96
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2021-03-11 20:39:34 +0530

    Create first.c
    This is first basic program in C language for printing the statement.

---
+++ first.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    // variables 
+    int top;
+    int Top;
+    int errorCode;
+    // 'J' is a char
+    
+    printf("Hello, I am learning C !!");
+    return 0;
+}
