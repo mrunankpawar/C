bb7badd2b3cd020bb74f3e09c6f089bc1ed5755a
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2021-03-13 08:49:41 +0530

    Convert temperature from Celsius to Fahrenheit

---
+++ 06_practice3.c
@@ -0,0 +1,10 @@
+//Convert celcius  into farenheit
+#include <stdio.h>
+
+int main()
+{
+    float celsius = 37;
+    float far = (celsius * 9 / 5) + 32;
+    printf("The value of this celsius temperature in farenheit is %f \n :", far);
+    return 0;
+}
