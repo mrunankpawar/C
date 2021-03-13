48e5717da46718b334c3ae38862fe5df7e0ad5f1
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2021-03-13 08:48:41 +0530

    Area of Circle and Volume of Cylinder

---
+++ 05_practice2.c
@@ -0,0 +1,14 @@
+//Area of circle and volume of cylinder
+#include <stdio.h>
+
+int main()
+{
+    int radius = 5;
+    float pi = 3.14;
+    printf("The area of circle is %f \n", pi * radius * radius);
+
+    int height = 3;
+    printf("The volume of cylinder is %f \n: ", pi * radius * radius * height);
+
+    return 0;
+}
