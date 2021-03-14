69b463bbbe5779ce252d1f75b4096787b704a1df
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2021-03-14 08:50:52 +0530

    Determine whether a number is divisible by 97 or not

---
+++ 11_practice5.c
@@ -0,0 +1,11 @@
+//Determine whether a number is divisible by 97 or not
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter the number : \n");
+    scanf("%d", &num);
+    printf("Divisibility test returns : %d \n", num % 97);
+    return 0;
+}
