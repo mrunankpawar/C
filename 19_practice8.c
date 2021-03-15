ae4cc397429aa9436b30ceafa790472dca2d4003
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2021-03-15 17:21:55 +0530

    Program to calculate tax to be paid by an indivi

---
+++ 19_practice8.c
@@ -0,0 +1,29 @@
+//Income tax
+#include <stdio.h>
+
+int main()
+{
+    float tax = 0, income;
+
+    printf("Enter your income :\n");
+    scanf("%f", &income);
+
+    if (income >= 250000 && income <= 500000)
+    {
+        tax = tax + 0.05 * (income - 2500000);
+    }
+
+    if (income >= 500000 && income <= 1000000)
+    {
+        tax = tax + 0.20 * (income - 500000);
+    }
+
+    if (income >= 1000000)
+    {
+        tax = tax + 0.30 * (income - 1000000);
+    }
+
+    printf("The income tax to be paid is %f\n", tax);
+
+    return 0;
+}
