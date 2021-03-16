471281413e27e5aa8de713800339d51a03a53751
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2021-03-16 16:35:10 +0530

    for loop

---
+++ 27_forloop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    //for(initialize; condition; updation
+    // {
+    //      code;
+    //      code;
+    // }
+    
+    for (int i = 0; i <= 10; i++)
+    {
+        printf("The value of i is : %d\n", i);
+    }
+
+    return 0;
+}
