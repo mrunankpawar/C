09cc00cc3f4d48f75efaa0107f97a189888f87b4
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2021-03-16 21:06:30 +0530

    Print n natural numbers in reverse order

---
+++ 29_practice14.c
@@ -0,0 +1,18 @@
+//Write a program to print n natural numbers in reverse order
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter the value of n :\n");
+    scanf("%d", &n);
+
+    for (int i = n; i; i--)
+    {
+        printf("The value of n is: %d\n", i);
+    }
+
+    return 0;
+}
