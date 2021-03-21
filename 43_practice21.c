cb02d3ff87bcac871db4ff9c4adf4e087546d58e
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2021-03-21 15:08:40 +0530

    Calculate average of 3 numbers

---
+++ 43_practice21.c
@@ -0,0 +1,22 @@
+//Calculate average of 3 numbers
+
+#include<stdio.h> 
+
+float average(int a, int b, int c);
+
+int main()
+{
+    int a, b, c;
+    printf("Enter the value of a :\n");
+    scanf("%d", &a);
+    printf("Enter the value of b :\n");
+    scanf("%d", &b);
+    printf("Enter the value of c :\n");
+    scanf("%d", &c);
+    printf("The value of average is : %f\n", average(a,b,c));
+}
+
+float average(int a, int b, int c){
+    float result = (float)(a + b + c)/3;
+    return result;
+}
