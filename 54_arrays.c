86fe273c3ae07a8840776438de6c1041cc609414
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2021-03-23 12:30:24 +0530

    arrays

---
+++ 54_arrays.c
@@ -0,0 +1,19 @@
+#include<stdio.h> 
+
+int main()
+{
+    // int marks1, marks2, marks3, marks4;
+    // marks1 = 5;
+    // marks2 = 10;
+    // marks3 = 15;
+    // marks4 = 20;
+    
+    int marks[4];
+    marks[0] = 5;
+    marks[1] = 10;
+    marks[2] = 15;
+    marks[3] = 20;
+
+
+    return 0;
+}
