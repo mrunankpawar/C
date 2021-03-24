fd7d6390c1409986c972117b52c2accdba7aac29
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2021-03-24 15:17:43 +0530

    array initialization

---
+++ 57_array_initialization.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+
+int main()
+{
+    int a[] = {5, 10, 15, 20};
+    printf("The value of a[0] is : %d\n", a[0]);
+    printf("The value of a[1] is : %d\n", a[1]);
+    printf("The value of a[2] is : %d\n", a[2]);
+    printf("The value of a[3] is : %d\n", a[3]);
+
+    float b[] = {5.5, 10.13, 56.9};
+    printf("The value of b[0] is : %.2f\n", b[0]);
+    printf("The value of b[1] is : %.2f\n", b[1]);
+    printf("The value of b[2] is : %.2f\n", b[2]);
+    return 0;
+}
