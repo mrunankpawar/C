27fecfdda4d295d4ff8185a5ad32b427966c5ab4
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2021-03-25 21:14:22 +0530

    Reversing the passed array

---
+++ 62_practice31.c
@@ -0,0 +1,29 @@
+//Reverse a passed array
+#include<stdio.h> 
+
+void reverse(int *arr, int n){
+    int temp;
+    for (int i = 0; i < (n/2); i++)
+    {
+        temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+    }
+}
+
+int main()
+{
+    int arr[] = {1,2,3,4,5,6};
+    for (int i = 0; i < 6; i++)
+    {
+        printf("The value of element before reversing is %d\n", i+1);
+    }
+    reverse(arr, 6);
+    printf("REVERSING THE ARRAY !!\n");
+    for (int i = 0; i < 6; i++)
+    {
+        printf("The value of element after reversing is %d\n", arr[i]);
+    }
+
+    return 0;
+}
