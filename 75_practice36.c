07b0667b3cb9e397ba89471e04174d03b25a64f1
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2021-03-26 15:27:33 +0530

    own strlen() function

---
+++ 75_practice36.c
@@ -0,0 +1,20 @@
+//Create own strlen()
+#include<stdio.h> 
+
+int strlen(char *st){
+    char *ptr = st;
+    int len = 0;
+    while (*ptr!='\0')
+    {
+        len++;
+        ptr++;
+    }
+    return len;
+}
+int main()
+{
+    char st[] = "Mrunank";
+    int l = strlen(st);
+    printf("The length of the string is : %d\n", l);
+    return 0;
+}
